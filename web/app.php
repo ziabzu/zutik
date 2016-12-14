c201b6b1c8f8ab7914b17ae841df50d90efa0c26
ziabzu <ziabzu@gmail.com>
2016-12-14 08:44:11 +0100

    Update app.php

--- web/app.php
+++ web/app.php
@@ -7,7 +7,7 @@ $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
 
-//Its her Zia
+//Its her Zia - 3
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
