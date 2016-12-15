22397a8a81c0add449cd9fd0258e7faa27063449
ziabzu <ziabzu@gmail.com>
2016-12-15 12:49:39 +0100

    My first commit

--- web/app.php
+++ web/app.php
@@ -6,6 +6,8 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
+
+// My first file
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
