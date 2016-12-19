6cf3105f9781fc5df197fbce070eee65089a1583
ziabzu <ziabzu@gmail.com>
2016-12-19 16:57:24 +0100

    just a test

--- src/AppBundle/Controller/AboutController.php
+++ src/AppBundle/Controller/AboutController.php
@@ -14,6 +14,7 @@ class AboutController extends Controller
 	*/
 	public function index() {
 
+		// Zia testing here
 		return $this->render('static/about.html.twig', array(
             'number' => 123,
         ));
